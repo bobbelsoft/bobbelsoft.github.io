d71b6fef49257c304be7a751f1b8d177c4b63391
Dude <144910371+Pizzaeat99@users.noreply.github.com>
2025-02-07 18:09:00 +0400

    Delete js.js

--- js.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.dropdown-btn').addEventListener('click', function() {
-    var dropdownMenu = document.querySelector('.dropdown-menu');
-    dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
-});
